2feae4fad2d5e4f314ec0df56fd5e5ae64340b53
Clemo97 <lumumbaclement@gmail.com>
2023-04-17 21:31:04 +0300

    github profile viewer complete

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  username: 'angular',
+  apiUrl: 'https://api.github.com'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  username: 'codediodeio',
+  apiUrl: 'https://api.github.com'
+};
+
